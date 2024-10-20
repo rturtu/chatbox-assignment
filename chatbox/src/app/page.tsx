3ab35a4a03579159844b6777c300b929340f7874
Razvan Turturica <razvan.turtu@yahoo.com>
2024-10-20 14:04:50 +0300

    Fix chatbox dynamic loading to allow sessionStorage

--- chatbox/src/app/page.tsx
+++ chatbox/src/app/page.tsx
@@ -1,6 +1,10 @@
-import { ChatBox } from "./components/ChatBox";
+'use client';
+import dynamic from 'next/dynamic';
+// lazy load the ChatBox component to allow sessionStorage to be used
+const ChatBox = dynamic(() => import('@/app/components/ChatBox'), { ssr: false });
 
 export default function Home() {
+
   return (
     <div className="flex items-center justify-center h-screen" >
       <h1 className="text-3xl" >Chatbox - Razvan Turturica</h1>
