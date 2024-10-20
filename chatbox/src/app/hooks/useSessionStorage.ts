49f49c3a87af5077ca734c77867fdb56db42756e
Razvan Turturica <razvan.turtu@yahoo.com>
2024-10-20 13:57:04 +0300

    Add useSessionStorage hook for managing session storage in the ChatBox widget

---
+++ chatbox/src/app/hooks/useSessionStorage.ts
@@ -0,0 +1,21 @@
+'use client';
+import React, { Dispatch, SetStateAction } from "react";
+
+export const useSessionStorage = <T extends unknown>(key: string, initialValue: T):[T, Dispatch<SetStateAction<T>>] => {
+
+    const getStoredValue = () => {
+        const value = sessionStorage.getItem(key);
+        if (value) {
+            return JSON.parse(value);
+        }
+        return initialValue;
+    }
+
+    const [storedValue, setStoredValue] = React.useState<T>(getStoredValue);
+
+    React.useEffect(() => {
+        sessionStorage.setItem(key, JSON.stringify(storedValue));
+    }, [key, storedValue]);
+
+    return [storedValue as T, setStoredValue];
+}
